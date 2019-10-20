b3c3ec22b75904bb53bd64602d9860f6d59fab98
MargaritaOt <margo_19@mail.ru>
2019-10-20 21:43:25 +0300

    Личный проект: надо подкачаться

---
+++ js/map.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+  window.adsLoad = function () {
+    window.load(function (adsArray) {
+      var fragment = document.createDocumentFragment();
+      for (var k = 0; k < adsArray.length; k++) {
+        var pin = window.pinElement.cloneNode(true);
+        var obj = adsArray[k];
+        pin.querySelector('img').src = obj.author.avatar;
+        pin.querySelector('img').alt = obj.offer.title;
+        pin.style.left = obj.location.x + 'px';
+        pin.style.top = obj.location.y + 'px';
+
+        fragment.appendChild(pin);
+      }
+
+      document.querySelector('.map__pins').appendChild(fragment);
+    });
+  };
+})();
---
+++ js/pin.js
@@ -0,0 +1,65 @@
+'use strict';
+
+(function () {
+
+  document.getElementById('address').value = 'x:603 y:237';
+
+  var map = document.querySelector('.map');
+  window.mapPinActive = document.querySelector('.map__pin--main');
+
+  window.mapPinActive.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.ENTER_KEYCODE) {
+      window.adsLoad();
+      map.classList.remove('map--faded');
+      window.adForm.classList.remove('ad-form--disabled');
+    }
+  });
+
+  window.mapPinActive.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    map.classList.remove('map--faded');
+    document.getElementById('address').value = 'x:599 y:448';
+    window.adsLoad();
+  });
+
+  var button = document.querySelector('.map__pin');
+  var onButtonMove = function (evt) {
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      document.getElementById('address').value = 'x:' + button.style.top + '  ' + 'y:' + button.style.left;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      button.style.top = (button.offsetTop - shift.y) + 'px';
+      button.style.left = (button.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  button.addEventListener('mousedown', onButtonMove);
+
+})();
